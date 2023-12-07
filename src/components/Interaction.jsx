be4a2ff393b991bdfdfa3ea9d51c127f89220ff7
Amr Elsehety <amr.abdelazeem22@gmail.com>
2023-12-08 00:51:42 +0300

    Create Interaction component

---
+++ src/components/Interaction.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+
+export default function Interaction(){
+    return <section className="interaction">
+    <p>Page Views</p>
+    <img src="../images/icon-facebook.svg" alt="icon" />
+    <h3>87</h3>
+    <div className="percentage">
+        <img src="../images/icon-up.svg" alt="icon" />
+        <p>3%</p>
+    </div>
+    </section>
+}
