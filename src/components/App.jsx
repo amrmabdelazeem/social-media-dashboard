96f106ad10f18ee0f643b272389817abbfd81488
Amr Elsehety <amr.abdelazeem22@gmail.com>
2023-12-07 19:01:35 +0300

    Create twitter card

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -8,7 +8,7 @@ export default function App(){
         <Heading/>
         <section>
             <Account src="../images/icon-facebook.svg" title="@nathanf" followerNum="1987" postTitle="FOLLOWERS" arrow="../images/icon-up.svg" progress="12 Today"/>
-            
+            <Account src="../images/icon-twitter.svg" title="@nathanf" followerNum="1044" postTitle="FOLLOWERS" arrow="../images/icon-up.svg" progress="99 Today"/>
         </section>
     </>
 }
