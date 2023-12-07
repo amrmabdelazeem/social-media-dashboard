568636f2a337084ab296b89f8ab5cc31161326e6
Amr Elsehety <amr.abdelazeem22@gmail.com>
2023-12-07 18:01:44 +0300

    Populate Account component

--- src/components/Account.jsx
+++ src/components/Account.jsx
@@ -1,5 +1,18 @@
 import React from "react";
 
-export default function Account(){
-    
+export default function Account() {
+  return (
+    <div className="account-layer">
+      <div className="info">
+        <img src="../images/icon-facebook.svg" alt="facebook-icon" />
+        <p>@nathanf</p>
+      </div>
+      <h2>1987</h2>
+      <p>Followers</p>
+      <div className="progress">
+        <img src="../images/icon-up.svg" alt="up-icon" />
+        <p> 12 Today</p>
+      </div>
+    </div>
+  );
 }
