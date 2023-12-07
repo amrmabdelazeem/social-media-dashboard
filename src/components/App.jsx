c9cf12da17efaa8c9012d59908c84ad31e9a4de4
Amr Elsehety <amr.abdelazeem22@gmail.com>
2023-12-07 18:49:32 +0300

    Add Component Account to main

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
 import Heading from './Heading';
+import Account from './Account';
 
 
 export default function App(){
     return <>
         <Heading/>
+        <section>
+            <Account/>
+        </section>
     </>
 }
