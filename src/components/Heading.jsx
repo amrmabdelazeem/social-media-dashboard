dcf2394b4616d8f361b13b138e4aadeec04e7e1e
Amr Elsehety <amr.abdelazeem22@gmail.com>
2023-12-07 17:18:56 +0300

    Add theme area

--- src/components/Heading.jsx
+++ src/components/Heading.jsx
@@ -1,9 +1,18 @@
-import React from 'react';
+import React from "react";
 
-
-export default function Heading(){
-    return <section>
-    <h1>Social Media Dashboard</h1>
-    <p>Total Followers: 23,004</p>
-</section>
+export default function Heading() {
+  return (
+    <section>
+      <div className="title">
+        <h1>Social Media Dashboard</h1>
+        <p>Total Followers: 23,004</p>
+      </div>
+      <div className="theme-area">
+      <p>Dark Mode</p>
+      <div className="toggle-element">
+        <button/>
+      </div>
+      </div>
+    </section>
+  );
 }
