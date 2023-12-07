16502881a571a689f2f5aabdbf88891b2480508e
Amr Elsehety <amr.abdelazeem22@gmail.com>
2023-12-07 19:03:53 +0300

    Create youtube card

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -10,6 +10,7 @@ export default function App(){
             <Account src="../images/icon-facebook.svg" title="@nathanf" followerNum="1987" postTitle="FOLLOWERS" arrow="../images/icon-up.svg" progress="12 Today"/>
             <Account src="../images/icon-twitter.svg" title="@nathanf" followerNum="1044" postTitle="FOLLOWERS" arrow="../images/icon-up.svg" progress="99 Today"/>
             <Account src="../images/icon-instagram.svg" title="@realnathanf" followerNum="11K" postTitle="FOLLOWERS" arrow="../images/icon-up.svg" progress="1099 Today"/>
+            <Account src="../images/icon-youtube.svg" title="Nathan F." followerNum="8239" postTitle="SUBSCRIBERS" arrow="../images/icon-down.svg" progress="144 Today"/>
         </section>
     </>
 }
