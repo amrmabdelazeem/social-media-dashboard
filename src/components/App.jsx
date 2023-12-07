7bc7e910f7e5545cd37bffcdeef229e14a1e1002
Amr Elsehety <amr.abdelazeem22@gmail.com>
2023-12-07 18:59:53 +0300

    Add props to Account component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -7,7 +7,8 @@ export default function App(){
     return <>
         <Heading/>
         <section>
-            <Account/>
+            <Account src="../images/icon-facebook.svg" title="@nathanf" followerNum="1987" postTitle="FOLLOWERS" arrow="../images/icon-up.svg" progress="12 Today"/>
+            
         </section>
     </>
 }
